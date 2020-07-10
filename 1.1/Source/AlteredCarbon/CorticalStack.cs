dc3370a6d2528ebeef4f6ffed632c3dd80824c35
Taranchuk <nfhfyxer@gmail.com>
2020-07-10 22:39:30 +0300

    hostility and work priority fix

--- 1.1/Source/AlteredCarbon/CorticalStack.cs
+++ 1.1/Source/AlteredCarbon/CorticalStack.cs
@@ -9,6 +9,7 @@ namespace AlteredCarbon
     public class CorticalStack : ThingWithComps
     {
         public Name name;
+        public int hostilityMode;
         public Faction faction;
         public List<Thought_Memory> thoughts;
         public List<Trait> traits;
@@ -37,18 +38,34 @@ namespace AlteredCarbon
         public void SavePawnToCorticalStack(Pawn pawn)
         {
             Log.Message("Saving " + pawn);
+            Log.Message(" - SavePawnToCorticalStack - this.name = pawn.Name; - 2", true);
             this.name = pawn.Name;
+            Log.Message(" - SavePawnToCorticalStack - this.hostilityMode = (int)pawn.playerSettings.hostilityResponse; - 3", true);
+            this.hostilityMode = (int)pawn.playerSettings.hostilityResponse;
+            Log.Message(" - SavePawnToCorticalStack - this.thoughts = pawn.needs.mood.thoughts.memories.Memories; - 4", true);
             this.thoughts = pawn.needs.mood.thoughts.memories.Memories;
+            Log.Message(" - SavePawnToCorticalStack - this.faction = pawn.Faction; - 5", true);
             this.faction = pawn.Faction;
+            Log.Message(" - SavePawnToCorticalStack - this.traits = pawn.story.traits.allTraits; - 6", true);
             this.traits = pawn.story.traits.allTraits;
+            Log.Message(" - SavePawnToCorticalStack - this.relations = pawn.relations.DirectRelations; - 7", true);
             this.relations = pawn.relations.DirectRelations;
+            Log.Message(" - SavePawnToCorticalStack - this.skills = pawn.skills.skills; - 8", true);
             this.skills = pawn.skills.skills;
+            Log.Message(" - SavePawnToCorticalStack - this.childhood = pawn.story.childhood.identifier; - 9", true);
             this.childhood = pawn.story.childhood.identifier;
-            this.adulthood = pawn.story.adulthood.identifier;
+            Log.Message(" - SavePawnToCorticalStack - this.adulthood = pawn.story.adulthood.identifier; - 10", true);
+            if (pawn.story.adulthood != null)
+            {
+                this.adulthood = pawn.story.adulthood.identifier;
+            }
+            Log.Message(" - SavePawnToCorticalStack - this.priorities = new Dictionary<WorkTypeDef, int>(); - 11", true);
             this.priorities = new Dictionary<WorkTypeDef, int>();
+            Log.Message(" - SavePawnToCorticalStack - foreach (WorkTypeDef w in DefDatabase<WorkTypeDef>.AllDefs) - 12", true);
             foreach (WorkTypeDef w in DefDatabase<WorkTypeDef>.AllDefs)
             {
-                priorities[w] = pawn.workSettings.GetPriority(w);
+                Log.Message(" - SavePawnToCorticalStack - priorities[w] = pawn.workSettings.GetPriority(w); - 13", true);
+                this.priorities[w] = pawn.workSettings.GetPriority(w);
             }
             this.hasPawn = true;
         }
@@ -97,25 +114,35 @@ namespace AlteredCarbon
                 pawn.skills.skills.Add(newSkill);
             }
 
+            pawn.playerSettings.hostilityResponse = (HostilityResponseMode)this.hostilityMode;
+
             //pawn.skills.skills = this.skills;
 
+            Backstory newChildhood = null;
+            BackstoryDatabase.TryGetWithIdentifier(this.childhood, out newChildhood, true);
+            pawn.story.childhood = newChildhood;
+            if (this.adulthood?.Length > 0)
+            {
+                Backstory newAdulthood = null;
+                BackstoryDatabase.TryGetWithIdentifier(this.adulthood, out newAdulthood, true);
+                pawn.story.adulthood = newAdulthood;
+            }
+            else
+            {
+                pawn.story.adulthood = null;
+            }
+            pawn.Notify_DisabledWorkTypesChanged();
             foreach (var priority in priorities)
             {
                 pawn.workSettings.SetPriority(priority.Key, priority.Value);
             }
-            Backstory newChildhood = null;
-            BackstoryDatabase.TryGetWithIdentifier(this.childhood, out newChildhood, true);
-            pawn.story.childhood = newChildhood;
-            
-            Backstory newAdulthood = null;
-            BackstoryDatabase.TryGetWithIdentifier(this.adulthood, out newAdulthood, true);
-            pawn.story.adulthood = newAdulthood;
         }
 
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_Deep.Look<Name>(ref this.name, "name", new object[0]);
+            Scribe_Values.Look<int>(ref this.hostilityMode, "hostilityMode");
             Scribe_Collections.Look<Thought_Memory>(ref this.thoughts, "thoughts");
             Scribe_References.Look<Faction>(ref this.faction, "faction", true);
             Scribe_Values.Look<string>(ref this.childhood, "childhood", null, false);
