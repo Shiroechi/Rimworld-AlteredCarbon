171d62e41b9291595e4d50a05652de9dc3bad4c7
Taranchuk <nfhfyxer@gmail.com>
2020-07-14 17:28:52 +0300

    update

---
+++ 1.1/Source/AlteredCarbon/HediffCompProperties_CorticalStack - Copy.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace AlteredCarbon
+{
+	public class ThoughtWorker_ColonyUsesStacks : ThoughtWorker
+	{
+		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			if (p.story.traits.HasTrait(AlteredCarbonDefOf.AC_AntiStack))
+			{
+				foreach (var pawn in p.Map.mapPawns.AllPawns.Where(x => x.IsColonist))
+				{
+					if (pawn != p && ACUtils.ACTracker.pawnsWithStacks.Contains(pawn))
+					{
+						return ThoughtState.ActiveDefault;
+					}
+				}
+			}
+			return ThoughtState.Inactive;
+		}
+	}
+}
