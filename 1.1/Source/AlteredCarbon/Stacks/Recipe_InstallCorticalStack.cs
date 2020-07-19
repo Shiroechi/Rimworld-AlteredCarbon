6a0fbce3c1404bb05b7a67205555648b8942c464
Taranchuk <nfhfyxer@gmail.com>
2020-07-19 19:12:23 +0300

    fixed psylink bug

--- 1.1/Source/AlteredCarbon/Stacks/Recipe_InstallCorticalStack.cs
+++ 1.1/Source/AlteredCarbon/Stacks/Recipe_InstallCorticalStack.cs
@@ -7,117 +7,112 @@ using Verse;
 
 namespace AlteredCarbon
 {
-	public class Recipe_InstallCorticalStack : Recipe_Surgery
-	{
-		public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
-		{
-			return MedicalRecipesUtility.GetFixedPartsToApplyOn(recipe, pawn, delegate (BodyPartRecord record)
-			{
-				IEnumerable<Hediff> source = pawn.health.hediffSet.hediffs.Where((Hediff x) => x.Part == record);
-				if (source.Count() == 1 && source.First().def == recipe.addsHediff)
-				{
-					return false;
-				}
-				if (record.parent != null && !pawn.health.hediffSet.GetNotMissingParts().Contains(record.parent))
-				{
-					return false;
-				}
-				return (!pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(record) || pawn.health.hediffSet.HasDirectlyAddedPartFor(record)) ? true : false;
-			});
-		}
+    public class Recipe_InstallCorticalStack : Recipe_Surgery
+    {
+        public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+        {
+            return MedicalRecipesUtility.GetFixedPartsToApplyOn(recipe, pawn, delegate (BodyPartRecord record)
+            {
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - GetPartsToApplyOn - if (!pawn.health.hediffSet.GetNotMissingParts().Contains(record)) - 1", true);
+                if (!pawn.health.hediffSet.GetNotMissingParts().Contains(record))
+                {
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - GetPartsToApplyOn - return false; - 2", true);
+                    return false;
+                }
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - GetPartsToApplyOn - if (pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(record)) - 3", true);
+                if (pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(record))
+                {
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - GetPartsToApplyOn - return false; - 4", true);
+                    return false;
+                }
+                return (!pawn.health.hediffSet.hediffs.Any((Hediff x) => x.Part == record && (x.def == recipe.addsHediff || !recipe.CompatibleWithHediff(x.def)))) ? true : false;
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - GetPartsToApplyOn - }); - 6", true);
+            });
+        }
 
         public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
         {
-            bool flag = MedicalRecipesUtility.IsClean(pawn, part);
-            bool flag2 = !PawnGenerator.IsBeingGenerated(pawn) && IsViolationOnPawn(pawn, part, Faction.OfPlayer);
+            Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if (billDoer != null) - 7", true);
             if (billDoer != null)
             {
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill)) - 8", true);
                 if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
                 {
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - foreach (var i in ingredients) - 9", true);
                     foreach (var i in ingredients)
                     {
+                        Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if (i is CorticalStack c) - 10", true);
                         if (i is CorticalStack c)
                         {
+                            Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - c.stackCount = 1; - 11", true);
                             c.stackCount = 1;
+                            Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - Traverse.Create(c).Field(\"mapIndexOrState\").SetValue((sbyte)-1); - 12", true);
                             Traverse.Create(c).Field("mapIndexOrState").SetValue((sbyte)-1);
+                            Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - GenPlace.TryPlaceThing(c, billDoer.Position, billDoer.Map, ThingPlaceMode.Near); - 13", true);
                             GenPlace.TryPlaceThing(c, billDoer.Position, billDoer.Map, ThingPlaceMode.Near);
                         }
                     }
                     return;
                 }
                 TaleRecorder.RecordTale(TaleDefOf.DidSurgery, billDoer, pawn);
-                MedicalRecipesUtility.RestorePartAndSpawnAllPreviousParts(pawn, part, billDoer.Position, billDoer.Map);
-                if (flag && flag2 && part.def.spawnThingOnRemoved != null)
-                {
-                    ThoughtUtility.GiveThoughtsForPawnOrganHarvested(pawn);
-                }
-                if (flag2)
-                {
-                    ReportViolation(pawn, billDoer, pawn.FactionOrExtraHomeFaction, -70, "GoodwillChangedReason_NeedlesslyInstalledWorseBodyPart".Translate(recipe.addsHediff.label));
-                }
-            }
-            else if (pawn.Map != null)
-            {
-                MedicalRecipesUtility.RestorePartAndSpawnAllPreviousParts(pawn, part, pawn.Position, pawn.Map);
-            }
-            else
-            {
-                pawn.health.RestorePart(part);
             }
             var thing = ingredients.Where(x => x is CorticalStack).FirstOrDefault();
+            Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if (thing is CorticalStack corticalStack) - 17", true);
             if (thing is CorticalStack corticalStack)
             {
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - var hediff = HediffMaker.MakeHediff(recipe.addsHediff, pawn) as Hediff_CorticalStack; - 18", true);
                 var hediff = HediffMaker.MakeHediff(recipe.addsHediff, pawn) as Hediff_CorticalStack;
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - hediff.stackGroupID = corticalStack.stackGroupID; - 19", true);
                 hediff.stackGroupID = corticalStack.stackGroupID;
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - pawn.health.AddHediff(recipe.addsHediff, part); - 20", true);
                 pawn.health.AddHediff(recipe.addsHediff, part);
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if (corticalStack.hasPawn) - 21", true);
                 if (corticalStack.hasPawn)
                 {
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if (pawn.IsColonist) - 22", true);
                     if (pawn.IsColonist)
                     {
+                        Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - Find.StoryWatcher.statsRecord.Notify_ColonistKilled(); - 23", true);
                         Find.StoryWatcher.statsRecord.Notify_ColonistKilled();
                     }
                     PawnDiedOrDownedThoughtsUtility.TryGiveThoughts(pawn, null, PawnDiedOrDownedThoughtsKind.Died);
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - pawn.health.NotifyPlayerOfKilled(null, null, null); - 25", true);
                     pawn.health.NotifyPlayerOfKilled(null, null, null);
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - ACUtils.ACTracker.stacksIndex.Remove(corticalStack.pawnID + corticalStack.name); - 26", true);
                     ACUtils.ACTracker.stacksIndex.Remove(corticalStack.pawnID + corticalStack.name);
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - corticalStack.OverwritePawn(pawn); - 27", true);
                     corticalStack.OverwritePawn(pawn);
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - ACUtils.ACTracker.ReplaceStackWithPawn(corticalStack, pawn); - 28", true);
                     ACUtils.ACTracker.ReplaceStackWithPawn(corticalStack, pawn);
-            
+
                     var naturalMood = pawn.story.traits.GetTrait(TraitDefOf.NaturalMood);
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - var nerves = pawn.story.traits.GetTrait(TraitDefOf.Nerves); - 30", true);
                     var nerves = pawn.story.traits.GetTrait(TraitDefOf.Nerves);
-            
+
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - if ((naturalMood != null && naturalMood.Degree == -2) - 31", true);
                     if ((naturalMood != null && naturalMood.Degree == -2)
                             || pawn.story.traits.HasTrait(TraitDefOf.BodyPurist)
                             || (nerves != null && nerves.Degree == -2)
                             || pawn.story.traits.HasTrait(AlteredCarbonDefOf.AC_Sleever))
                     {
+                        Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - pawn.needs.mood.thoughts.memories.TryGainMemory(AlteredCarbonDefOf.AC_NewSleeveDouble); - 32", true);
                         pawn.needs.mood.thoughts.memories.TryGainMemory(AlteredCarbonDefOf.AC_NewSleeveDouble);
                     }
                     else
                     {
+                        Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - pawn.needs.mood.thoughts.memories.TryGainMemory(AlteredCarbonDefOf.AC_NewSleeve); - 33", true);
                         pawn.needs.mood.thoughts.memories.TryGainMemory(AlteredCarbonDefOf.AC_NewSleeve);
                     }
                 }
                 else
                 {
+                    Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - ACUtils.ACTracker.RegisterPawn(pawn); - 34", true);
                     ACUtils.ACTracker.RegisterPawn(pawn);
                 }
                 ACUtils.ACTracker.pawnsWithStacks.Add(pawn);
+                Log.Message("Recipe_InstallCorticalStack : Recipe_Surgery - ApplyOnPawn - ACUtils.ACTracker.TryAddRelationships(pawn); - 36", true);
                 ACUtils.ACTracker.TryAddRelationships(pawn);
             }
         }
-
-		public override bool IsViolationOnPawn(Pawn pawn, BodyPartRecord part, Faction billDoerFaction)
-		{
-			if ((pawn.Faction == billDoerFaction || pawn.Faction == null) && !pawn.IsQuestLodger())
-			{
-				return false;
-			}
-			if (recipe.addsHediff.addedPartProps != null && recipe.addsHediff.addedPartProps.betterThanNatural)
-			{
-				return false;
-			}
-			return HealthUtility.PartRemovalIntent(pawn, part) == BodyPartRemovalIntent.Harvest;
-		}
-	}
+    }
 }
-
