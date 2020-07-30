4ba2e9a0ef00e3395ad70c180dd11cd7a648c4dd
Taranchuk <nfhfyxer@gmail.com>
2020-07-30 09:53:05 +0300

    update

--- 1.1/Source/AlteredCarbon/HarmonyPatches/Patch_SpawnSetup.cs
+++ 1.1/Source/AlteredCarbon/HarmonyPatches/Patch_SpawnSetup.cs
@@ -30,5 +30,67 @@ namespace AlteredCarbon
             }
         }
     }
+
+    [HarmonyPatch(typeof(PawnTechHediffsGenerator))]
+    [HarmonyPatch("GenerateTechHediffsFor")]
+    public static class Patch_GenerateTechHediffsFor
+    {
+        public static bool Prefix(Pawn pawn, List<ThingDef> ___tmpGeneratedTechHediffsList)
+        {
+            Log.Message(" - Prefix - float partsMoney = pawn.kindDef.techHediffsMoney.RandomInRange; - 1", true);
+            float partsMoney = pawn.kindDef.techHediffsMoney.RandomInRange;
+            Log.Message(" - Prefix - int num = pawn.kindDef.techHediffsMaxAmount; - 2", true);
+            int num = pawn.kindDef.techHediffsMaxAmount;
+            Log.Message(" - Prefix - if (pawn.kindDef.techHediffsRequired != null) - 3", true);
+            if (pawn.kindDef.techHediffsRequired != null)
+            {
+                Log.Message(" - Prefix - foreach (ThingDef item in pawn.kindDef.techHediffsRequired) - 4", true);
+                foreach (ThingDef item in pawn.kindDef.techHediffsRequired)
+                {
+                    Log.Message(" - Prefix - partsMoney -= item.BaseMarketValue; - 5", true);
+                    partsMoney -= item.BaseMarketValue;
+                    Log.Message(" - Prefix - num--; - 6", true);
+                    num--;
+                    //PawnTechHediffsGenerator.InstallPart(pawn, item);
+                }
+            }
+            Log.Message(" - Prefix - if (pawn.kindDef.techHediffsTags == null || pawn.kindDef.techHediffsChance <= 0f) - 8", true);
+            if (pawn.kindDef.techHediffsTags == null || pawn.kindDef.techHediffsChance <= 0f)
+            {
+                Log.Message(" - Prefix - return false; - 9", true);
+                return false;
+            }
+            ___tmpGeneratedTechHediffsList.Clear();
+            for (int i = 0; i < num; i++)
+            {
+                Log.Message(" - Prefix - if (Rand.Value > pawn.kindDef.techHediffsChance) - 11", true);
+                if (Rand.Value > pawn.kindDef.techHediffsChance)
+                {
+                    Log.Message(" - Prefix - break; - 12", true);
+                    break;
+                }
+                IEnumerable<ThingDef> source = DefDatabase<ThingDef>.AllDefs.Where((ThingDef x) => x.isTechHediff && !___tmpGeneratedTechHediffsList.Contains(x) && x.BaseMarketValue <= partsMoney && x.techHediffsTags != null && pawn.kindDef.techHediffsTags.Any((string tag) => x.techHediffsTags.Contains(tag)) && (pawn.kindDef.techHediffsDisallowTags == null || !pawn.kindDef.techHediffsDisallowTags.Any((string tag) => x.techHediffsTags.Contains(tag))));
+                Log.Message(" - Prefix - if (source.Any()) - 14", true);
+                if (source.Any())
+                {
+                    foreach (var t in source)
+                    {
+                        Log.Message("Source: " + t, true);
+                    }
+                    Log.Message(" - Prefix - ThingDef thingDef = source.RandomElementByWeight((ThingDef w) => w.BaseMarketValue); - 15", true);
+                    ThingDef thingDef = source.RandomElementByWeight((ThingDef w) => w.BaseMarketValue);
+                    Log.Message(" - Prefix - partsMoney -= thingDef.BaseMarketValue; - 16", true);
+                    Log.Message("thingDef: " + thingDef, true);
+
+                    partsMoney -= thingDef.BaseMarketValue;
+                    //PawnTechHediffsGenerator.InstallPart(pawn, thingDef);
+                    Log.Message(" - Prefix - ___tmpGeneratedTechHediffsList.Add(thingDef); - 18", true);
+                    ___tmpGeneratedTechHediffsList.Add(thingDef);
+                }
+            }
+            return false;
+        }
+    }
+    
 }
 
