d0fb56e8bc50dc1a1e40ffa93e60343ef5956229
Taranchuk <nfhfyxer@gmail.com>
2020-08-16 20:06:29 +0300

    update

--- 1.2/Source/AlteredCarbon/HarmonyPatches/Patch_SpawnSetup.cs
+++ 1.2/Source/AlteredCarbon/HarmonyPatches/Patch_SpawnSetup.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
 using RimWorld;
+using RimWorld.QuestGen;
 using Verse;
+using static RimWorld.QuestGen.QuestGen_Pawns;
 
 namespace AlteredCarbon
 {
@@ -31,5 +33,44 @@ namespace AlteredCarbon
             }
         }
     }
+
+    [HarmonyPatch(typeof(QuestGen_Pawns))]
+    [HarmonyPatch("GetPawn")]
+    public static class GetPawnSetup
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Quest quest, GetPawnParms parms, Pawn __result)
+        {
+            if (__result.kindDef == PawnKindDefOf.Empire_Royal_Bestower)
+            {
+                ThingOwner<Thing> innerContainer = __result.inventory.innerContainer;
+                innerContainer.TryAdd(ThingMaker.MakeThing(AlteredCarbonDefOf.AC_EmptyCorticalStack), 1);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LordJob_BestowingCeremony))]
+    [HarmonyPatch("FinishCeremony")]
+    public static class FinishCeremony_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(LordJob_BestowingCeremony __instance, Pawn pawn)
+        {
+            if (__instance.bestower.kindDef == PawnKindDefOf.Empire_Royal_Bestower)
+            {
+                ThingOwner<Thing> innerContainer = __instance.bestower.inventory.innerContainer;
+                for (int num = innerContainer.Count - 1; num >= 0; num--)
+                {
+                    if (innerContainer[num].def == AlteredCarbonDefOf.AC_EmptyCorticalStack)
+                    {
+                        innerContainer.TryDrop(innerContainer[num], ThingPlaceMode.Near, out Thing lastResultingThing);
+                    }
+                }
+            }
+        }
+    }
+
+    
+    
 }
 
