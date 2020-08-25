1c2653bf041c7b86fd834232dbac79cd9802eeb1
Taranchuk <nfhfyxer@gmail.com>
2020-08-25 23:39:57 +0300

    update

---
+++ 1.2/Source/AlteredCarbon/AlteredCarbonMod.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace AlteredCarbon
+{
+    class AlteredCarbonMod : Mod
+    {
+        public static AlteredCarbonSettings settings;
+        public AlteredCarbonMod(ModContentPack pack) : base(pack)
+        {
+            settings = GetSettings<AlteredCarbonSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+
+        // Return the name of the mod in the settings tab in game
+        public override string SettingsCategory()
+        {
+            return "Altered Carbon";
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class DefsRemover
+    {
+        static DefsRemover()
+        {
+            if (!AlteredCarbonMod.settings.allowAC_Apparel_ProtectorateArmor)
+            {
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Apparel_ProtectorateArmor"));
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Apparel_ProtectorateArmorHelmet"));
+            }
+            if (!AlteredCarbonMod.settings.allowAC_Gun_BullpupPistol)
+            {
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Gun_BullpupPistol"));
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Bullet_BullpupPistol"));
+            }
+            if (!AlteredCarbonMod.settings.allowAC_Gun_MACRevolver)
+            {
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Gun_MACRevolver"));
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Bullet_MACRevolver"));
+            }
+            if (!AlteredCarbonMod.settings.allowAC_Gun_MACRifle)
+            {
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Gun_MACRifle"));
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Bullet_MACRifle"));
+            }
+            if (!AlteredCarbonMod.settings.allowAC_Gun_QuickfirePistol)
+            {
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Gun_QuickfirePistol"));
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Bullet_QuickfirePistol"));
+            }
+            if (!AlteredCarbonMod.settings.allowAC_Gun_ShockPDW)
+            {
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Gun_ShockPDW"));
+                DefDatabase<ThingDef>.AllDefsListForReading.Remove(ThingDef.Named("AC_Bullet_ShockPDW"));
+            }
+            if (AlteredCarbonMod.settings.allowStacksToBeStacked)
+            {
+                AlteredCarbonDefOf.AC_EmptyCorticalStack.stackLimit = 25;
+                AlteredCarbonDefOf.AC_EmptyCorticalStack.drawGUIOverlay = true;
+
+                AlteredCarbonDefOf.AC_FilledCorticalStack.stackLimit = 25;
+                AlteredCarbonDefOf.AC_FilledCorticalStack.drawGUIOverlay = true;
+            }
+        }
+    }
+}
---
+++ 1.2/Source/AlteredCarbon/AlteredCarbonSettings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace AlteredCarbon
+{
+    class AlteredCarbonSettings : ModSettings
+    {
+        public bool allowStacksToBeStacked = false;
+
+        public bool allowAC_Apparel_ProtectorateArmor = true;
+        public bool allowAC_Gun_BullpupPistol = true;
+        public bool allowAC_Gun_MACRevolver = true;
+        public bool allowAC_Gun_MACRifle = true;
+        public bool allowAC_Gun_QuickfirePistol = true;
+        public bool allowAC_Gun_ShockPDW = true;
+        public void ResetSavedDefs()
+        {
+            allowAC_Apparel_ProtectorateArmor = true;
+            allowAC_Gun_BullpupPistol = true;
+            allowAC_Gun_MACRevolver = true;
+            allowAC_Gun_MACRifle = true;
+            allowAC_Gun_QuickfirePistol = true;
+            allowAC_Gun_ShockPDW = true;
+        }
+
+        public override void ExposeData()
+        {
+
+            base.ExposeData();
+            Scribe_Values.Look(ref allowAC_Apparel_ProtectorateArmor, "allowAC_Apparel_ProtectorateArmor", true);
+            Scribe_Values.Look(ref allowAC_Gun_BullpupPistol, "allowAC_Gun_BullpupPistol", true);
+            Scribe_Values.Look(ref allowAC_Gun_MACRevolver, "allowAC_Gun_MACRevolver", true);
+            Scribe_Values.Look(ref allowAC_Gun_MACRifle, "allowAC_Gun_MACRifle", true);
+            Scribe_Values.Look(ref allowAC_Gun_QuickfirePistol, "allowAC_Gun_QuickfirePistol", true);
+            Scribe_Values.Look(ref allowAC_Gun_ShockPDW, "allowAC_Gun_ShockPDW", true);
+            Scribe_Values.Look(ref allowStacksToBeStacked, "allowStacksToBeStacked", true);
+
+        }
+
+        // Draw the actual settings window that shows up after selecting Z-Levels in the list
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("allowStacksToBeStacked".Translate(), ref allowStacksToBeStacked);
+            listingStandard.CheckboxLabeled("allowAC_Apparel_ProtectorateArmor".Translate(), ref allowAC_Apparel_ProtectorateArmor);
+            listingStandard.CheckboxLabeled("allowAC_Gun_BullpupPistol".Translate(), ref allowAC_Gun_BullpupPistol);
+            listingStandard.CheckboxLabeled("allowAC_Gun_MACRevolver".Translate(), ref allowAC_Gun_MACRevolver);
+            listingStandard.CheckboxLabeled("allowAC_Gun_MACRifle".Translate(), ref allowAC_Gun_MACRifle);
+            listingStandard.CheckboxLabeled("allowAC_Gun_QuickfirePistol".Translate(), ref allowAC_Gun_QuickfirePistol);
+            listingStandard.CheckboxLabeled("allowAC_Gun_ShockPDW".Translate(), ref allowAC_Gun_ShockPDW);
+            listingStandard.End();
+        }
+    }
+}
