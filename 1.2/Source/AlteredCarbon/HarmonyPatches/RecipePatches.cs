2206372f3e4e25d8a037538f6c24dbd51d7f4ce9
Taranchuk <nfhfyxer@gmail.com>
2020-09-01 11:06:50 +0300

    update

--- 1.2/Source/AlteredCarbon/HarmonyPatches/RecipePatches.cs
+++ 1.2/Source/AlteredCarbon/HarmonyPatches/RecipePatches.cs
@@ -12,11 +12,24 @@ namespace AlteredCarbon
 	{
 		private static bool Prefix(RecipeDef __instance, Thing thing, ref bool __result)
 		{
-			if (__instance == AlteredCarbonDefOf.AC_InstallEmptyCorticalStack && thing is Pawn pawn && pawn.IsEmptySleeve())
+			if (__instance == AlteredCarbonDefOf.AC_InstallEmptyCorticalStack && thing is Pawn pawn)
+			{
+				var option = __instance.GetModExtension<ExcludeRacesModExtension>();
+				if (option != null && option.racesToExclude.Contains(pawn.def.defName) || pawn.IsEmptySleeve())
+				{
+					__result = false;
+					return false;
+				}
+			}
+			else if (__instance == AlteredCarbonDefOf.AC_InstallCorticalStack && thing is Pawn pawn2)
             {
-				__result = false;
-				return false;
-            }
+				var option = __instance.GetModExtension<ExcludeRacesModExtension>();
+				if (option != null && option.racesToExclude.Contains(pawn2.def.defName))
+				{
+					__result = false;
+					return false;
+				}
+			}
 			return true;
 		}
 	}
